7ab90ff2012918ec0304c1807551883f40c04753
Vinay Reddy <vinay.sankalamaddi.31.2@mountblue.tech>
2025-04-28 18:51:53 +0530

    first commit

--- tasks/views.py
+++ tasks/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from .models import Task
 from .serializers import TaskSerializer
 
-# Create your views here.
+# Create your views here. 
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all().order_by('-created_at')
